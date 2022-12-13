925df0c9ffb1519bc70a3258fe338552620cd791
Tarsiciosh <63419165+Tarsiciosh@users.noreply.github.com>
2022-12-13 10:43:00 -0300

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Tarsicio Spraggon Hernández on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
